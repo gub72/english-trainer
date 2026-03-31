351966e1e0092087829770d7729dea5152f12f2b
unknown <gustavo.neves@infracommerce.com.br>
2026-03-31 18:18:21 -0300

    fix: ajuste back

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://english-trainer-fa5tuocd1-gub72s-projects.vercel.app',
+        changeOrigin: true,
+      },
+    },
+  },
 })
